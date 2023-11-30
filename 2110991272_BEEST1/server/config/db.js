f6f1731de14d9865ee77437f984892570d82d20b
skapoor950 <104484862+skapoor950@users.noreply.github.com>
2023-11-30 10:19:09 +0530

    Add files via upload

---
+++ 2110991272_BEEST1/server/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+//const config = require('config');
+const db = "mongodb://localhost:27017/beest1";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.log("error is here");
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
